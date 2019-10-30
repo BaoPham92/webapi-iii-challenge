278ba000ceffc47d6a894e968c3f5b49361ea952
GUL-Bao <BaoPham92@yahoo.com>
2019-10-30 16:10:56 -0400

    init commit

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// code away!
+const server = require('./server');
+
+// ? LISTENER
+const port = 5000;
+server.listen(port, () => console.log(`LISTENING TO ${port}`))
--- server.js
+++ server.js
@@ -1,13 +1,23 @@
-const express = 'express';
-
+// * SETTINGS & PKGS
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
 const server = express();
+const settings = [express.json(), morgan('combined'), helmet()]
 
+// * MIDDLEWARE-EXPRESS ROUTERS
+const postRouter = require('./posts/postRouter');
+const userRouter = require('./users/userRouter');
+
+// * BASE MIDDLEWARE
+server.use(settings);
+
+// * ROUTES
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
 //custom middleware
-
 function logger(req, res, next) {
 
 };
