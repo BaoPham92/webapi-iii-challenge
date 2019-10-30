62105690ea1962361830376996d4400a6e0446c4
GUL-Bao <BaoPham92@yahoo.com>
2019-10-30 16:52:20 -0400

    custom logger done

--- server.js
+++ server.js
@@ -3,23 +3,24 @@ const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const server = express();
-const settings = [express.json(), morgan('combined'), helmet()]
+
+//custom middleware
+function logger(req, res, next) {
+  console.log(`Logger is logging the method: ${req.method} to URL: ${req.url}`);
+  next();
+};
 
 // * MIDDLEWARE-EXPRESS ROUTERS
 const postRouter = require('./posts/postRouter');
 const userRouter = require('./users/userRouter');
 
 // * BASE MIDDLEWARE
+const settings = [express.json(), morgan('combined'), helmet(), logger]
 server.use(settings);
 
 // * ROUTES
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
-
-//custom middleware
-function logger(req, res, next) {
-
-};
 
 module.exports = server;
